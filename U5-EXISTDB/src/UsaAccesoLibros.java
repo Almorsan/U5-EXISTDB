1ef94220917301ad88b9d5396c535fbbf4debb7e
Almorsan <131298499+Almorsan@users.noreply.github.com>
2024-03-15 16:12:27 +0100

    Commit bueno

---
+++ U5-EXISTDB/src/UsaAccesoLibros.java
@@ -0,0 +1,8 @@
+public class UsaAccesoLibros {
+	public static void main(String[] args) {
+		AccesoLibros a = new AccesoLibros();
+		a.conectar();
+		a.consultarTitulos();
+		a.desconectar();
+	}
+}
